9c5863c9c8fc1ff359f617c0dc8444873460346f
KamalDGRT <kamaldgrt@gmail.com>
2022-06-25 13:21:15 +0530

    start with the storyboard part

---
+++ Viper_Basic_Slider/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Viper_Basic_Slider
+//
+//  Created by Kamal Sharma R on 22/06/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
